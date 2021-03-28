67f17a498b4455368ece64400493a7dbd0595e63
xjl <18253040259@163.com>
2021-03-28 14:52:12 +0800

    增加了查看文件功能

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -23,27 +23,24 @@ func main() {
 		fmt.Println("参数"+strconv.Itoa(idx)+":", args)
 	}
 
-	if len(os.Args) <= 1 {
+	if len(os.Args) <= 2 {
 		//upload("./file/file.txt")
-		postFile("./file/file.txt", "http://localhost:8080/upload")
+		//postFile("./file/file.txt", "http://localhost:8080/upload")
 		return
 	}
 	//command := strconv.Itoa(1)
 	command := os.Args[1]
-	fmt.Println(command)
-	fmt.Println("upload")
-	if command != "upload" {
-		fmt.Println("Command does not exist!")
+	if command == "upload" {
+		postFile("./file/file.txt", "http://localhost:8080/upload")
 		return
 	}
-	//filePath := strconv.Itoa(2)
-	filePath := os.Args[2]
-	if filePath == "" {
-		fmt.Println("file path is empty!")
+	if command == "ls" {
+		getDisk()
 		return
 	}
 }
 
+//上传文件
 func postFile(filename string, targetUrl string) error {
 	bodyBuf := &bytes.Buffer{}
 	bodyWriter := multipart.NewWriter(bodyBuf)
@@ -80,4 +77,19 @@ func postFile(filename string, targetUrl string) error {
 	fmt.Println(string(resp_body))
 
 	return nil
+}
+
+//获取网盘文件
+func getDisk() {
+	resp, err := http.Get("http://localhost:8080/ls")
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	fmt.Printf("%s\n", b)
 }
