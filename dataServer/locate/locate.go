c61b1244b5a98b52c1da18dbe8f065cbb62228a0
xjl <18253040259@163.com>
2021-05-09 11:45:28 +0800

    数据服务

---
+++ dataServer/locate/locate.go
@@ -0,0 +1,29 @@
+package locate
+
+import (
+	"XCloud/lib/rabbitmq"
+	"os"
+	"strconv"
+)
+
+func Locate(name string) bool {
+	_, err := os.Stat(name)
+	return !os.IsNotExist(err)
+}
+
+func StartLocate() {
+	q := rabbitmq.New(os.Getenv("RABBITMQ_SERVER"))
+	defer q.Close()
+	q.Bind("dataServers")
+	c := q.Consume()
+	for msg := range c {
+		objects, e := strconv.Unquote(string(msg.Body))
+		if e != nil {
+			panic(e)
+		}
+
+		if Locate(os.Getenv("STORAGE_ROOT") + "/objects/" + objects) {
+			q.Send(msg.ReplyTo, os.Getenv("LISTEN_ADDRESS"))
+		}
+	}
+}
