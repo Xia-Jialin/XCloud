3473b71195ed6debd195737357ca4b649f5c147d
Xiajialin <xjialin@yeah.net>
2021-05-23 20:29:16 +0800

    heartbeat.StartHeartbeat()向消息队列发送节点监听地址

--- dataServer/dataServer.go
+++ dataServer/dataServer.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"XCloud/dataServer/heartbeat"
+	"XCloud/dataServer/locate"
 	"XCloud/dataServer/objects"
 	"log"
 	"net/http"
@@ -8,6 +10,8 @@ import (
 )
 
 func main() {
+	go heartbeat.StartHeartbeat()
+	go locate.StartLocate()
 	http.HandleFunc("/objects/", objects.Handler)
 	log.Fatal(http.ListenAndServe(os.Getenv("LISTEN_ADDRESS"), nil))
 }
