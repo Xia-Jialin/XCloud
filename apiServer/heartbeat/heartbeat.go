1038dd75673488c5f6910edcedb5019c1c98ca00
xjl <18253040259@163.com>
2021-07-09 12:59:25 +0800

    更改环境变量名

--- apiServer/heartbeat/heartbeat.go
+++ apiServer/heartbeat/heartbeat.go
@@ -12,7 +12,7 @@ var dataServers = make(map[string]time.Time)
 var mutex sync.Mutex
 
 func ListenHeartbeat() {
-	q := rabbitmq.New(os.Getenv("RABBITMQ_SYRVER"))
+	q := rabbitmq.New(os.Getenv("RABBITMQ_SERVER"))
 	defer q.Close()
 	q.Bind("apiServers")
 	c := q.Consume()
