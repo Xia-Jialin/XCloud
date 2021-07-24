7fdd79b68ed28812d23826d10dc9805cbe12dfad
xjl <18253040259@163.com>
2021-07-24 23:10:17 +0800

    增加数据冗余和即时修复

--- apiServer/objects/put_stream.go
+++ apiServer/objects/put_stream.go
@@ -2,15 +2,15 @@ package objects
 
 import (
 	"XCloud/apiServer/heartbeat"
-	"XCloud/lib/objectstream"
+	"XCloud/lib/rs"
 	"fmt"
 )
 
-func putStream(hash string, size int64) (*objectstream.TempPutStream, error) {
-	server := heartbeat.ChooseRandomDataServer()
-	if server == "" {
-		return nil, fmt.Errorf("cannot find any dataServer")
+func putStream(hash string, size int64) (*rs.RSPutStream, error) {
+	servers := heartbeat.ChooseRandomDataServers(rs.ALL_SHARDS, nil)
+	if len(servers) != rs.ALL_SHARDS {
+		return nil, fmt.Errorf("cannot find enough dataServer")
 	}
 
-	return objectstream.NewTempPutStream(server, hash, size)
+	return rs.NewRSPutStream(servers, hash, size)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module XCloud
 
 go 1.16
 
-require github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271
+require (
+	github.com/klauspost/reedsolomon v1.9.12
+	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271
+)
